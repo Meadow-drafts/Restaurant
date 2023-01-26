b493ed6bdd7c75ad56571366d889b4e3c5cff222
Shirley Laurette <Shirleylaurette1@gmail.com>
2023-01-26 07:03:06 +0100

    _

--- src/pages/coffee/Hero.vue
+++ src/pages/coffee/Hero.vue
@@ -184,32 +184,127 @@
         </div>
       </div>
     </div>
-<!--About us Section-->
-    <div id="about" class=" ">
-      <div class=" h-full text-gray-800">
-        <div class="flex  flex-wrap  h-full">
-<!--          about us img-->
-          <div class="grow-0 shrink-1 md:shrink-0  lg:items-left relative  basis-auto w-50 mb-12 md:mb-0">
-            <img
-                src="https://images.unsplash.com/photo-1513267048331-5611cad62e41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNvZmZlZSUyMHNob3B8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
-                class="w-full"
-                alt="Sample image"
-            />
-          </div>
-          <!-- about us text card-->
-          <div class="rounded-md xl:ml-20 p-5 ml-24 mt-10 text-white  w-2/3 mb-12  absolute bg-black  ">
-          <h2 class="text-6xl text-yellow-200">Our Story</h2>
-            <p class="font-sans text-lg">On her way she met a copy. The copy warned the Little Blind Text,
-              that where it came from it would have been rewritten a thousand times and everything that was
-              left from its origin would be the word "and" and the Little Blind Text should turn around and
-              return to its own, safe country. But nothing the copy said could convince her and so it didn’t
-              take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole
-              and dragged her into their agency, where they abused her for their.</p>
+    <!--    info card counter section-->
+
+    <div class="py-5">
+      <main class="h-full overflow-y-auto">
+        <p class="text-4xl text-center font-medium m-5 border-b-2 border-gray-200 shadow-b-lg">Our Stats</p>
+        <div class="container mx-auto grid">
+          <!-- Cards -->
+          <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+            <!-- Card -->
+            <div style="height: 10rem;"
+                class="flex items-center p-4 bg-black rounded-lg shadow-xs"
+            >
+              <div
+                  class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full "
+              >
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                      d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
+                  ></path>
+                </svg>
+              </div>
+              <div>
+                <p
+                    class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
+                >
+                  My Blog Posts
+                </p>
+                <p
+                    class="text-lg font-semibold text-gray-200"
+                >
+                  10
+                </p>
+              </div>
+            </div>
+            <!-- Card -->
+            <div style="height: 10rem;"
+                class="flex items-center p-4 bg-black rounded-lg shadow-xs"
+            >
+              <div
+                  class="p-3 mr-4 text-green-500 bg-green-100 rounded-full"
+              >
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                      fill-rule="evenodd"
+                      d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+                      clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <div>
+                <p
+                    class="mb-2 text-sm font-medium text-gray-600"
+                >
+                  My Forum Posts
+                </p>
+                <p
+                    class="text-lg font-semibold text-gray-200 "
+                >
+                  24
+                </p>
+              </div>
+            </div>
+            <!-- Card -->
+            <div style="height: 10rem;"
+                class="flex items-center p-4 rounded-lg shadow-xs bg-black"
+            >
+              <div
+                  class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full "
+              >
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                      fill-rule="evenodd"
+                      d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
+                      clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <div>
+                <p
+                    class="mb-2 text-sm font-medium text-gray-600"
+                >
+                  My Tutorials
+                </p>
+                <p
+                    class="text-lg font-semibold text-gray-200"
+                >
+                  376
+                </p>
+              </div>
+            </div>
+            <!-- Card -->
+            <div style="height: 10rem;"
+                class="flex items-center p-4 bg-black rounded-lg shadow-xs "
+            >
+              <div
+                  class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full"
+              >
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                      d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+                  ></path>
+                </svg>
+              </div>
+              <div>
+                <p
+                    class="mb-2 text-sm font-medium text-gray-600"
+                >
+                  My Purchases
+                </p>
+                <p
+                    class="text-lg font-semibold text-gray-200"
+                >
+                  35
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </div>
-<!--    our services-->
+    <!--    our services-->
   <div>
     <img :src="services" alt="">
   </div>
