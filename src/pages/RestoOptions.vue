fae847981ae33f2b0ab58187ba49b7f2766fda59
Shirley Laurette <Shirleylaurette1@gmail.com>
2023-02-19 16:38:48 +0200

    _

--- src/pages/RestoOptions.vue
+++ src/pages/RestoOptions.vue
@@ -1,5 +1,193 @@
 <template>
-  <div class="grid h-screen place-items-center">
+
+  <section>
+		<div>
+      <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      class="absolute block w-full"
+      style="margin: auto; z-index: -10;"
+      height="680"
+      preserveAspectRatio="none"
+      viewBox="0 0 1920 880"
+    >
+      <g transform="translate(960,440) scale(1,1) translate(-960,-440)">
+        <linearGradient
+          id="lg-0.047955344060927496"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+        >
+          <stop stop-color="hsl(217, 88%, 33.7%)" offset="0"></stop>
+          <stop stop-color="hsl(217, 88%, 75.1%)" offset="1"></stop>
+        </linearGradient>
+        <path d="" fill="#00E400" opacity="0.4">
+          <animate
+            attributeName="d"
+            dur="33.333333333333336s"
+            repeatCount="indefinite"
+            keyTimes="0;0.333;0.667;1"
+            calcmod="spline"
+            keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
+            begin="0s"
+            values="M0 0L 0 804.2328934685746Q 320 597.3613372284876  640 571.0708916590191T 1280 512.0661063245175T 1920 301.8788007488083L 1920 0 Z;M0 0L 0 877.6839081951588Q 320 668.0720922803877  640 649.0018928349388T 1280 328.7087077664202T 1920 162.95038242563396L 1920 0 Z;M0 0L 0 724.9886210051687Q 320 661.4364572061575  640 623.2173947479624T 1280 359.20353038907734T 1920 135.51673041732283L 1920 0 Z;M0 0L 0 804.2328934685746Q 320 597.3613372284876  640 571.0708916590191T 1280 512.0661063245175T 1920 301.8788007488083L 1920 0 Z"
+          ></animate>
+        </path>
+        <path d="" fill="#00E400" opacity="0.4">
+          <animate
+            attributeName="d"
+            dur="33.333333333333336s"
+            repeatCount="indefinite"
+            keyTimes="0;0.333;0.667;1"
+            calcmod="spline"
+            keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
+            begin="-6.666666666666667s"
+            values="M0 0L 0 765.7607191473613Q 320 641.7853945676919  640 624.2534689988059T 1280 365.27264408032966T 1920 190.38947978522663L 1920 0 Z;M0 0L 0 842.1984196370487Q 320 570.6690721707517  640 540.6844954979398T 1280 439.92879442880593T 1920 200.29713960445451L 1920 0 Z;M0 0L 0 796.6802345094818Q 320 721.9216894353016  640 696.8815669355181T 1280 373.6367381440213T 1920 196.63169821789495L 1920 0 Z;M0 0L 0 765.7607191473613Q 320 641.7853945676919  640 624.2534689988059T 1280 365.27264408032966T 1920 190.38947978522663L 1920 0 Z"
+          ></animate>
+        </path>
+        <path d="" fill="#00E400" opacity="0.4">
+          <animate
+            attributeName="d"
+            dur="33.333333333333336s"
+            repeatCount="indefinite"
+            keyTimes="0;0.333;0.667;1"
+            calcmod="spline"
+            keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
+            begin="-13.333333333333334s"
+            values="M0 0L 0 801.7562714943509Q 320 634.0247183381232  640 605.7090791951217T 1280 503.9393370140325T 1920 224.7551247480177L 1920 0 Z;M0 0L 0 821.0401780336218Q 320 670.8690783540507  640 637.0744123031742T 1280 456.40745286432224T 1920 278.1294357804296L 1920 0 Z;M0 0L 0 744.0534225112256Q 320 637.6425395409125  640 593.2079605185819T 1280 457.03995196824286T 1920 254.87693899994804L 1920 0 Z;M0 0L 0 801.7562714943509Q 320 634.0247183381232  640 605.7090791951217T 1280 503.9393370140325T 1920 224.7551247480177L 1920 0 Z"
+          ></animate>
+        </path>
+        <path d="" fill="#5ed16a" opacity="0.4">
+          <animate
+            attributeName="d"
+            dur="33.333333333333336s"
+            repeatCount="indefinite"
+            keyTimes="0;0.333;0.667;1"
+            calcmod="spline"
+            keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
+            begin="-20s"
+            values="M0 0L 0 817.8603658675457Q 320 592.9404308081629  640 559.1126621853513T 1280 428.9912604821798T 1920 209.017381620229L 1920 0 Z;M0 0L 0 802.0504889976935Q 320 561.3963273210122  640 537.6024084387631T 1280 430.41283267566695T 1920 256.1972069733954L 1920 0 Z;M0 0L 0 789.4448177495887Q 320 561.9675446430498  640 531.6192318019404T 1280 414.76018143244175T 1920 265.9163329632971L 1920 0 Z;M0 0L 0 817.8603658675457Q 320 592.9404308081629  640 559.1126621853513T 1280 428.9912604821798T 1920 209.017381620229L 1920 0 Z"
+          ></animate>
+        </path>
+        <path d="" fill="#5ed16a" opacity="0.1">
+          <animate
+            attributeName="d"
+            dur="33.333333333333336s"
+            repeatCount="indefinite"
+            keyTimes="0;0.333;0.667;1"
+            calcmod="spline"
+            keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
+            begin="-26.666666666666668s"
+            values="M0 0L 0 844.0541574423102Q 320 623.0697081316591  640 592.8483890737847T 1280 469.85448734523794T 1920 190.81850676853674L 1920 0 Z;M0 0L 0 871.4928294956283Q 320 618.9784567388518  640 593.1183717103518T 1280 376.5051942642811T 1920 141.32293927545027L 1920 0 Z;M0 0L 0 782.0118384610068Q 320 727.3267836497654  640 694.0476176759635T 1280 518.1545471640493T 1920 276.0053882957168L 1920 0 Z;M0 0L 0 844.0541574423102Q 320 623.0697081316591  640 592.8483890737847T 1280 469.85448734523794T 1920 190.81850676853674L 1920 0 Z"
+          ></animate>
+        </path>
+      </g>
+    </svg>
+			<div class="container grid grid-cols-2 items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
+				<h1 class="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-900">Aren't You Hungry ??</h1>
+				<p class="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Your Favourite meal served or Delivered to you hot and fresh</p>
+				<div class="flex flex-wrap justify-center">
+					<button type="button" class="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-gray-800 dark:text-gray-50">Make a Reservation</button>
+					<button type="button" class="px-8 py-3 m-2 text-lg border rounded dark:border-gray-700 dark:text-gray-900">See Restaurants</button>
+				</div>
+			</div>
+		</div>
+    
+    <!-- Cards -->
+    <div class="grid grid-cols-3">
+      <div class="w-5/6 mx-auto mb-12 -mt-12 rounded-lg shadow-lg lg:-mt-32 bg-white p-5">
+      <img src="https://source.unsplash.com/random/480x320" alt=""  class="h-20 rounded-full w-20 my-5 mx-auto">
+      <h4 class="font-bold text-2xl text-center">Select A Restaurant</h4>
+      <p class="mt-3">There are a couple of restaurant from which you can make a choice and place you command</p>
+
+    </div>
+    
+    <div class="w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-32 bg-white p-5">
+      <img src="https://source.unsplash.com/random/480x320" alt="" class="h-20 rounded-full w-20 my-5 mx-auto">
+      <h4 class="font-bold text-2xl text-center">Available Reservations</h4>
+      <p class="mt-3">You could choose to Dine In</p>
+      <p class="">You could choose to Dine on delivery</p>
+
+    </div>
+    <div class="w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-32 bg-white p-5">
+      <img src="https://source.unsplash.com/random/480x320" alt="" class="h-20 rounded-full w-20 my-5 mx-auto">
+      <h4 class="font-bold text-2xl text-center">SFast Delivery</h4>
+      <p class="mt-3">Irrespective of the restaurant, we make sure the delivery is fast</p>
+
+    </div>
+    </div>
+
+    <!-- About us -->
+    <div class="text-center my-10 text-green-600 border-b-2 w-1/2  border-green-600 font-bold text-2xl">About Us</div>
+    <div class="container grid grid-cols-2 items-center px-4 py-16 lg:w-1/2 mx-auto text-center rounded-lg shadow-lg md:px-10 lg:px-32 bg-gray-200">
+      <p class="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Your Favourite meal served or Delivered to you hot and fresh</p>
+      <img src="https://source.unsplash.com/random/480x320" alt=""  class="mx-auto text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl">
+        <img src="https://source.unsplash.com/random/480x320" alt=""  class="mx-auto text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl">
+				<p class="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Your Favourite meal served or Delivered to you hot and fresh</p>
+		</div>
+    <div class="text-center my-10 text-green-600 border-b-2 w-1/2 border-green-600 font-bold text-2xl">Meals you could find . . .</div>
+    <div class="text-right my-10 text-green-600 mx-10 font-bold text-2xl">Restautant one . . .</div>
+
+
+    <!-- Cards -->
+    <div class="grid grid-cols-3">
+      <div class="w-5/6 mx-auto mb-12 ounded-lg shadow-lg  bg-white p-5">
+      <img src="https://source.unsplash.com/random/480x320" alt=""  class="h-20 rounded-full w-20 my-5 mx-auto">
+      <div class="grid grid-cols-2">
+        <h4 class="font-bold text-2xl text-center">Food Name</h4>
+        <h4 class="font-bold text-2xl text-center">Price</h4>
+      </div>
+      <p class="mt-3">There are a couple of restaurant from which you can make a choice and place you command</p>
+      <button class="rounded-full bg-black text-white hover:bg-white hover:text-black hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                      <line x1="12" y1="5" x2="12" y2="19"></line>
+                      <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+      </button>
+    </div>
+    
+      <div class="w-5/6 mx-auto mb-12 ounded-lg shadow-lg  bg-white p-5">
+      <img src="https://source.unsplash.com/random/480x320" alt=""  class="h-20 rounded-full w-20 my-5 mx-auto">
+      <div class="grid grid-cols-2">
+        <h4 class="font-bold text-2xl text-center">Food Name</h4>
+        <h4 class="font-bold text-2xl text-center">Price</h4>
+      </div>
+      <p class="mt-3">There are a couple of restaurant from which you can make a choice and place you command</p>
+      <button class="rounded-full bg-black text-white hover:bg-white hover:text-black hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                      <line x1="12" y1="5" x2="12" y2="19"></line>
+                      <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+      </button>
+    </div>
+    
+      <div class="w-5/6 mx-auto mb-12 ounded-lg shadow-lg  bg-white p-5">
+      <img src="https://source.unsplash.com/random/480x320" alt=""  class="h-20 rounded-full w-20 my-5 mx-auto">
+      <div class="grid grid-cols-2">
+        <h4 class="font-bold text-2xl text-center">Food Name</h4>
+        <h4 class="font-bold text-2xl text-center">Price</h4>
+      </div>
+      <p class="mt-3">There are a couple of restaurant from which you can make a choice and place you command</p>
+      <button class="rounded-full bg-black text-white hover:bg-white hover:text-black hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                      <line x1="12" y1="5" x2="12" y2="19"></line>
+                      <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+      </button>
+    </div>
+    
+    
+    </div>
+
+    <firstOption/>
+    <secondOption/>
+    
+	</section>
+
+
+
+  <!-- <div class="grid h-screen place-items-center">
     <div class="rounded-lg border-2 border-gray-200 bg-gray-300 p-10 mb-4 shadow-2xl hover:bg-gray-200">
       <h3 class="mb-10 text-3xl font-medium text-yellow-900">
         Which Restaurant will you like to visit today?
@@ -13,8 +201,8 @@
       />
       </div>
      
-      <ul v-for="(data,index) in restaurants" :key="data.id" class="flex w-full flex-col m-5" >       
-        <button button @click="RestoChoice(index)">
+      <ul v-for="(data, index) in restaurants" :key="data.id" class="flex w-full flex-col m-5" >       
+        <button button @click="RestoChoice(data.id)">
           <li>
             <input
                 type="radio"
@@ -46,13 +234,17 @@
                 ></path>
               </svg>
             </label>
+            <p>{{ data.email.split("@")[0] }}</p>
           </li>
         </button>
-      </ul>
-    </div>
-  </div>
+        <router-link :to="link">one</router-link>
+      </ul>  </div>
+  </div> -->
 </template>
 <script>
+import firstOption from "./components/firstOption.vue";
+import secondOption from "./components/secondOption.vue";
+
 import axios from 'axios';
 import { LoopingRhombusesSpinner } from 'epic-spinners'
 export default {
@@ -63,39 +255,45 @@ export default {
       hosting: "hosting-small",
       Resto_name:'',
       restaurants:[],
+      link:"",
     }
   },
   components:{
     LoopingRhombusesSpinner,
+    firstOption,
+    secondOption,
   }
   ,
-  async mounted(){
-    this.isLoading = true;
-    await axios.get("http://localhost:8000/restaurant"
-    ).then((response) => {
-    console.log(response.data);
-    this.isLoading = false;
-    this.restaurants = response.data
-    for(let i=0; i<this.restaurants.length; i++){
-      console.log(this.restaurants[i].email.split("@"))
-      // this.route = this.restaurants[i].email.split("@")[1]
+//   async mounted(){
+//     this.isLoading = true;
+//     await axios.get("http://localhost:8000/restaurant"
+//     ).then((response) => {
+//     console.log(response.data);
+//     this.isLoading = false;
+//     this.restaurants = response.data
+//     for(let i=0; i<this.restaurants.length; i++){
+//       console.log(this.restaurants[i].email.split("@")[0])
+//       this.link = this.restaurants[i].email.split("@")[0]
+//       // this.route = this.restaurants[i].email.split("@")[1]
       
-;
-    }
+// ;
+//     }
 
-    })
-  },
+//     })
+//   },
   methods: {
     changeHosting(hosting){
       this.hosting = hosting
     },
-    RestoChoice(index){
+    RestoChoice(id){
       this.isLoading = true;
-      console.log(index)
-      if(index===0){
+      console.log(id)
+      if(id===1){
         this.$router.push({path: '/expresso'})
-      }else if(index === 1) {
+      }else {
+        
         this.$router.push({path: '/dine'})
+        console.log(id);
       }     
       this.isLoading = false;
 
---
+++ src/pages/components/firstOption.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <div class="text-right my-10 text-green-600 mx-10 font-bold text-2xl">Fast Food . . .</div>
+
+
+<!-- Cards -->
+<div  class="grid grid-cols-3">
+  <div v-for="data in restaurant_meals" :key="data" class="w-5/6 mx-auto mb-12 rounded-lg shadow-lg  bg-white p-5">
+  <img :src="data.img_url" alt=""  class="rounded-full w-28 h-28 my-5 mx-auto">
+  <div class="grid grid-cols-2 p-2">
+    <h4 class="font-bold text-2xl text-left">{{data.name}}</h4>
+    <h4 class="font-bold text-2xl text-center">{{data.price}} XAF</h4>
+  </div>
+  <p class="mt-3">{{ data.description }}</p>
+  <button class="rounded-full bg-black text-white hover:bg-white hover:text-black hover:shadow-xl focus:outline-none w-10 h-10 flex ml-auto transition duration-300">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-current m-auto">
+                  <line x1="12" y1="5" x2="12" y2="19"></line>
+                  <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
+  </button>
+</div>
+
+
+
+</div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default{
+    "name":"firstOption",
+    data(){
+        return{
+            restaurant_meals:[],
+        }
+    },
+    async mounted(){
+        await axios.get('http://localhost:8000/meal/showby/1'
+        ).then((response)=>{
+            console.log(response.data)
+            this.restaurant_meals = response.data
+        })
+
+    },
+}
+
+</script>
